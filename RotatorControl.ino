6431ab602b23b753d7e759e1f94d7f348cd28910
Max Kendall <66796793+mpkendall@users.noreply.github.com>
2025-11-01 12:41:16 -0400

    Refactor motor control limits and add calibration endpoints

--- RotatorControl.ino
+++ RotatorControl.ino
@@ -4,12 +4,16 @@
 #include <WebServer.h>
 #include "config.h"
 #include "motor.h"
-#include "esp_adc_cal.h"
 
 WebServer server(80);
 
-MotorControl azimuthMotor(M1P1, M1P2, ENA, AZPOT, POT_AZ_MIN, POT_AZ_MAX, POT_AZ_LIMIT_MIN, POT_AZ_LIMIT_MAX);
-MotorControl elevationMotor(M2P1, M2P2, ENB, ELPOT, POT_EL_MIN, POT_EL_MAX, POT_EL_LIMIT_MIN, POT_EL_LIMIT_MAX);
+int az_limit_min = POT_AZ_LIMIT_MIN;
+int az_limit_max = POT_AZ_LIMIT_MAX;
+int el_limit_min = POT_EL_LIMIT_MIN;
+int el_limit_max = POT_EL_LIMIT_MAX;
+
+MotorControl azimuthMotor(M1P1, M1P2, ENA, AZPOT, POT_AZ_MIN, POT_AZ_MAX, az_limit_min, az_limit_max);
+MotorControl elevationMotor(M2P1, M2P2, ENB, ELPOT, POT_EL_MIN, POT_EL_MAX, el_limit_min, el_limit_max);
 
 Direction motorDirection;
 
@@ -32,6 +36,15 @@ void setup() {
 
   server.on("/", handleRoot);
   server.on("/command", handleCommand);
+  server.on("/calibrate", handleCalibrate);
+  server.on("/status", []() {
+    Bearing b = getCurrentBearing();
+    String status = "Azimuth: " + String(b.azimuth) + ", Elevation: " + String(b.elevation);
+    server.send(200, "text/plain", status);
+  });
+  server.on("/ping", []() {
+    server.send(200, "text/plain", "pong");
+  });
   server.begin();
 }
 
@@ -46,6 +59,39 @@ void loop() {
       elevationMotor.setDirection(stop);
     }
   }
+}
+
+void updateMotorLimits() {
+  azimuthMotor = MotorControl(M1P1, M1P2, ENA, AZPOT, POT_AZ_MIN, POT_AZ_MAX, az_limit_min, az_limit_max);
+  elevationMotor = MotorControl(M2P1, M2P2, ENB, ELPOT, POT_EL_MIN, POT_EL_MAX, el_limit_min, el_limit_max);
+}
+
+void handleCalibrate() {
+  bool updated = false;
+  String msg = "";
+  if (server.hasArg("az_limit_min")) {
+    az_limit_min = server.arg("az_limit_min").toInt();
+    updated = true;
+  }
+  if (server.hasArg("az_limit_max")) {
+    az_limit_max = server.arg("az_limit_max").toInt();
+    updated = true;
+  }
+  if (server.hasArg("el_limit_min")) {
+    el_limit_min = server.arg("el_limit_min").toInt();
+    updated = true;
+  }
+  if (server.hasArg("el_limit_max")) {
+    el_limit_max = server.arg("el_limit_max").toInt();
+    updated = true;
+  }
+  if (updated) {
+    updateMotorLimits();
+    msg = "Calibration updated. ";
+  }
+  msg += "az_limit_min=" + String(az_limit_min) + ", az_limit_max=" + String(az_limit_max);
+  msg += ", el_limit_min=" + String(el_limit_min) + ", el_limit_max=" + String(el_limit_max);
+  server.send(200, "text/plain", msg);
 }
 
 void handleRoot() {
@@ -75,10 +121,6 @@ void handleCommand() {
     server.send(400, "text/plain", "Invalid azimuth or elevation value");
     return;
   }
-  if (azVal < POT_AZ_LIMIT_MIN || azVal > POT_AZ_LIMIT_MAX || elVal < POT_EL_LIMIT_MIN || elVal > POT_EL_LIMIT_MAX) {
-    server.send(400, "text/plain", "Coordinates out of range");
-    return;
-  }
 
   String response = "azimuth: " + az + ", elevation: " + el;
   targetAzimuth = azVal;
@@ -86,6 +128,8 @@ void handleCommand() {
   setMotorCoords(targetAzimuth, targetElevation);
   server.send(200, "text/plain", response);
 }
+
+
 
 Bearing getCurrentBearing() {
   Bearing b;
@@ -97,19 +141,23 @@ Bearing getCurrentBearing() {
 void setMotorCoords(int targetAz, int targetEl) {
   targetAz += AZ_OFFSET;
   targetEl += EL_OFFSET;
+  if(targetAz > 360) targetAz -= 360;
+  if(targetAz < 0) targetAz += 360;
   Bearing currentBearing = getCurrentBearing();
   if(targetAz > currentBearing.azimuth) {
-    azimuthMotor.setDirection(forward);
+    REVERSE_AZ ? azimuthMotor.setDirection(backward) : azimuthMotor.setDirection(forward);
   } else if (targetAz < currentBearing.azimuth) {
-    azimuthMotor.setDirection(backward);
+    REVERSE_AZ ? azimuthMotor.setDirection(forward) :  azimuthMotor.setDirection(backward);
   } else {
     azimuthMotor.setDirection(stop);
   }
 
+  if(targetEl > 180) targetEl = 180;
+  if(targetEl < 0) targetEl = 0;
   if(targetEl > currentBearing.elevation) {
-    elevationMotor.setDirection(forward);
+    REVERSE_EL ? elevationMotor.setDirection(backward) : elevationMotor.setDirection(forward);
   } else if (targetEl < currentBearing.elevation) {
-    elevationMotor.setDirection(backward);
+    REVERSE_EL ? elevationMotor.setDirection(forward) : elevationMotor.setDirection(backward);
   } else {
     elevationMotor.setDirection(stop);
   }
